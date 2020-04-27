9c90ae2287833b789ba0a1b768285735410e0ad9
hiank <kids.hiank@hotmail.com>
2020-04-27 16:28:08 +0800

    format text

--- net/runtime.go
+++ net/runtime.go
@@ -7,11 +7,9 @@ import (
 
 //Runtime 运行时
 type Runtime struct {
-
 	context.Context
-	Close 	context.CancelFunc
+	Close context.CancelFunc
 }
-
 
 var _singleRuntime *Runtime
 var _singleRuntimeOnce sync.Once
@@ -19,11 +17,11 @@ var _singleRuntimeOnce sync.Once
 //GetRuntime 获得Runtime
 func GetRuntime() *Runtime {
 
-	_singleRuntimeOnce.Do(func ()  {
-		
+	_singleRuntimeOnce.Do(func() {
+
 		_singleRuntime = new(Runtime)
 		_singleRuntime.Context, _singleRuntime.Close = context.WithCancel(context.Background())
-		go func ()  {
+		go func() {
 			<-_singleRuntime.Done()
 			_singleRuntime = nil
 			_singleRuntimeOnce = sync.Once{}
@@ -31,6 +29,3 @@ func GetRuntime() *Runtime {
 	})
 	return _singleRuntime
 }
-
-
-
